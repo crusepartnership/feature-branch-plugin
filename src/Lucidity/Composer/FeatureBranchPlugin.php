73d6b2d12803d27b936eac8017356e6408d1553f
Tristan Kenney <tristan@kenney.co>
2015-10-14 14:52:06 +1100

    Added Plugin file

---
+++ src/Lucidity/Composer/FeatureBranchPlugin.php
@@ -0,0 +1,61 @@
+<?php
+namespace Lucidity\Composer;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\InstallerEvent;
+use Composer\Installer\InstallerEvents;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class FeatureBranchPlugin implements PluginInterface, EventSubscriberInterface
+{
+    protected $composer;
+
+    protected $io;
+
+    /**
+     * Apply plugin modifications to composer
+     *
+     * @param Composer    $composer
+     * @param IOInterface $io
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $this->composer = $composer;
+        $this->io = $io;
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * The array keys are event names and the value can be:
+     *
+     * * The method name to call (priority defaults to 0)
+     * * An array composed of the method name to call and the priority
+     * * An array of arrays composed of the method names to call and respective
+     *   priorities, or 0 if unset
+     *
+     * For instance:
+     *
+     * * array('eventName' => 'methodName')
+     * * array('eventName' => array('methodName', $priority))
+     * * array('eventName' => array(array('methodName1', $priority), array('methodName2'))
+     *
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            InstallerEvents::PRE_DEPENDENCIES_SOLVING => ['resolveFeatureBranch', 0]
+        ];
+    }
+
+    /**
+     * @param InstallerEvent $event
+     */
+    public function resolveFeatureBranch(InstallerEvent $event)
+    {
+        print_r($event);
+    }
+}
